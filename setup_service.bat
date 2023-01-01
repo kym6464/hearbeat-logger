6b0f606f015906d942b5226367ab335c8aa75d07
Kamal Muradov <muradovkamal10@gmail.com>
2023-01-01 14:04:53 -0500

    Update setup_service.bat
    fix typo

--- setup_service.bat
+++ setup_service.bat
@@ -1,8 +1,8 @@
 :: Assumes nssm.exe is in PATH
 
-set PROJET_ROOT=%~dp0
+set PROJECT_ROOT=%~dp0
 set SERVICE="heartbeat"
-set PYTHON_EXEC="%PROJET_ROOT%env\Scripts\python.exe"
+set PYTHON_EXEC="%PROJECT_ROOT%env\Scripts\python.exe"
 set ENTRYPOINT="%PROJECT_ROOT%heartbeat.py"
 
 :: Remove any existing service
@@ -10,7 +10,7 @@ nssm remove %SERVICE% confirm
 
 :: Install service
 nssm install %SERVICE% %PYTHON_EXEC% ".\heartbeat.py" ".\gcloud-logger-key.json"
-nssm set %SERVICE% AppDirectory %PROJET_ROOT%
+nssm set %SERVICE% AppDirectory %PROJECT_ROOT%
 nssm set %SERVICE% Description "Writes heartbeat log to Google Cloud Logging on a schedule"
 nssm set %SERVICE% Start "SERVICE_DELAYED_AUTO_START"
 
